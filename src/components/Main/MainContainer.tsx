2cafe18e289da8dc65bf0e2e86ec9f61c28eaed3
Dmitry <dmitry@cojam.ru>
2021-03-13 16:26:23 +0500

    уточнил StartDate
    + перенос переменных в useEffect

--- src/components/Main/MainContainer.tsx
+++ src/components/Main/MainContainer.tsx
@@ -6,13 +6,18 @@ import type { TimeDiffType } from './logic/getTimeDiff';
 import Main from './Main';
 
 const MainContainer: React.FC = () => {
-    const StartDate = '2021-02-01T19:00:00Z'; // 2 февраля 2021 гоа
-    const EndDate = '2023-08-01T19:00:00.000Z'; // 2 августа 2023 года
-
     const [PastTimeDiff, setPastTimeDiff] = useState<TimeDiffType | undefined>();
     const [LeftTimeDiff, setLeftTimeDiff] = useState<TimeDiffType | undefined>();
 
     useEffect(() => {
+        // считаем от этого события https://zona.media/online/2021/01/18/navapolice#38248
+        // условная дата - 18 января 2021, 16:50 МСК
+        const StartDate = '2021-01-18T13:00:00.000Z';
+
+        // TODO: уточнить дату
+        // // 2 августа 2023 года
+        const EndDate = '2023-08-01T19:00:00.000Z';
+
         const setter = () => {
             setPastTimeDiff(getTimeDiff(StartDate, new Date().toISOString()));
             setLeftTimeDiff(getTimeDiff(new Date().toISOString(), EndDate));
